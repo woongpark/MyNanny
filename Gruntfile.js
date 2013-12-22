3afaccfea55773b40049029c7d555aac610c683b
Daehyung <ldhyung@kaist.ac.kr>
2013-12-22 13:38:53 +0900

    gruntfile add

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          base: '.',
+          port: 8000,
+          keepalive: true,
+          open: 'http://localhost:8000',
+          hostname: "*"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Default task(s).
+  grunt.registerTask('startup', ['connect:server']);
+};
